9e8ccf1a39e8067c6bae6e040d8cd7ebcfcc84fc
Saem <ottosigma@gmail.com>
2017-01-23 08:48:38 +0200

    moved mocking from service to dao

---
+++ src/main/java/com/sameperson/dao/MockDao.java
@@ -0,0 +1,21 @@
+package com.sameperson.dao;
+
+import com.sameperson.model.Message;
+import com.sameperson.model.Profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockDao {
+
+    private static Map<Long, Message> messages = new HashMap<>();
+    private static Map<Long, Profile> profiles = new HashMap<>();
+
+    public static Map<Long, Message> getMessages() {
+        return messages;
+    }
+
+    public static Map<Long, Profile> getProfiles() {
+        return profiles;
+    }
+}
