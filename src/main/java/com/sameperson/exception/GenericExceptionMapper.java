b24c90ad098fe9c9e95c8e1a0a70642f52c6bae9
Saem <ottosigma@gmail.com>
2017-01-29 15:57:44 +0200

    generic exception mapper

---
+++ src/main/java/com/sameperson/exception/GenericExceptionMapper.java
@@ -0,0 +1,20 @@
+package com.sameperson.exception;
+
+import com.sameperson.model.ErrorMessage;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    public Response toResponse(Throwable e) {
+        ErrorMessage errorMessage = new ErrorMessage(e.getMessage(), 500, "null");
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(errorMessage)
+                .build();
+    }
+
+}
